a457bd996f12c154f1af8f8384303518ebe9c592
Xavier <xavier.rouah@intempora.com>
2014-11-20 17:23:28 +0100

    First EAR version trigger org.apache.webbeans.exception.definition.DuplicateDefinitionException at deployment (still some issues with Primefaces)

---
+++ TomEE-JSF-EAR/TomEE-JSF-EAR-war/src/java/org/tomeejsfear/test/DialogView.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.tomeejsfear.test;
+
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import org.primefaces.context.RequestContext;
+ 
+//@javax.faces.bean.ManagedBean(name = "dfDialogView")
+//@javax.faces.bean.ViewScoped
+@javax.inject.Named("dfDialogView")
+@javax.faces.view.ViewScoped
+public class DialogView {
+         
+    String createTime;
+
+    public String getCreateTime() {
+        return createTime;
+    }
+    
+    String param1;
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String param1) {
+        this.param1 = param1;
+    }
+    
+    Boolean actionViewCalled = false;
+
+    public Boolean getActionViewCalled() {
+        return actionViewCalled;
+    }
+
+    public void setActionViewCalled(Boolean actionViewCalled) {
+        this.actionViewCalled = actionViewCalled;
+    }
+   
+    public void viewAction()
+    {
+        actionViewCalled = true;
+    }
+    
+    @PostConstruct
+    public void onPostConstruct()
+    {
+        Calendar c = Calendar.getInstance();
+        createTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "DialogView.onPostConstruct() @ " + createTime);
+    }
+     
+    public void close() {
+        RequestContext.getCurrentInstance().closeDialog(null);
+    }
+}
---
+++ TomEE-JSF-EAR/TomEE-JSF-EAR-war/src/java/org/tomeejsfear/test/Dialogs.java
@@ -0,0 +1,54 @@
+package org.tomeejsfear.test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+ 
+@javax.inject.Named("dfView")
+@javax.faces.view.ViewScoped
+public class Dialogs {
+    
+    String createTime;
+
+    public String getCreateTime() {
+        return createTime;
+    }
+    
+    @PostConstruct
+    public void onPostConstruct()
+    {
+        Calendar c = Calendar.getInstance();
+        createTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Dialogs.onPostConstruct() @ " + createTime);
+        
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bean POST CONSTRUCT", "");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    
+    public void open() {
+        
+        
+        List<String> paramList = new ArrayList<String>();
+        paramList.add("PARENT CREATED AT " + createTime);
+        Map<String, List<String>> paramMap = new HashMap<String, List<String>>();
+        paramMap.put("param1", paramList);
+        
+        RequestContext.getCurrentInstance().openDialog("dialogContent",null, paramMap);
+    }
+     
+    public void onClose(SelectEvent event) {
+        //Car car = (Car) event.getObject();
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Dialog closed", "Event:" + event);
+         
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
